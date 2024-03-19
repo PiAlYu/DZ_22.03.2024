b9d923ee80e4e8775efddd82526132718d425f38
PiAlYu <157111194+PiAlYu@users.noreply.github.com>
2024-03-19 21:20:17 +0300

    Update 20.py

--- 20.py
+++ 20.py
@@ -1,11 +1,15 @@
+from math import ceil
+from functools import lru_cache
+
+@lru_cache(None)
 def f(n, h):
-    if n == 1: return h % 2
+    if n == 1: return h % 2 == 0
     if h % 2 == 0:
-        return all(f(i, h + 1) for i in range(n // 2, n))
-    return any(f(i, h + 1) for i in range(n // 2, n))
+        return all(f(i, h + 1) for i in range(ceil(n / 2), n))
+    return any(f(i, h + 1) for i in range(ceil(n / 2), n))
 
 k = 0
 for j in range(2, 100):
-    if f(j, 0) == 1:
+    if f(j, 0):
         k += 1
 print(k)
